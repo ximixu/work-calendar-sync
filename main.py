a3aca209d8e1449c2b03f8491c0f8ad1fc96dc70
ximixu <mxu@live.com>
2024-12-30 21:39:46 -0800

    update main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import login
+import json_to_ical
 
 def main():
-    driver = login.get_authenticated_driver()
+    login.get_schedule()
+    json_to_ical("schedule.json")
 
 if __name__ == "__main__":
     main()
