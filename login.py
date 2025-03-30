995e071e59b3459e11fb6cf50f9b5bb76b2af741
ximixu <mxu@live.com>
2025-03-30 19:54:10 +0000

    change headless mode to try and get cron to run

--- login.py
+++ login.py
@@ -26,7 +26,7 @@ target_url = config["target_url"]
 temp_user_data_dir = tempfile.mkdtemp()
 
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
